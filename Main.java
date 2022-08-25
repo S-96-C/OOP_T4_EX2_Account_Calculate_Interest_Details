9378c03830270ad954e25c12388ae742b68b5910
Shehan Cooray <109017782+S-96-C@users.noreply.github.com>
2022-08-26 00:56:23 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Account acc1 = new FixedDepositAccount();//fixed deposit object
+	 	Account acc1 = new FixedDepositAccount();//fixed deposit object
 		SavingAccount acc2 = new SavingAccount();//savingAccount object
 		
 		acc1.deposit(10000);
@@ -13,6 +13,6 @@ public class Main {
     		acc1.display();
 		acc2.display();
 		
-  }
+  	}
 
 }
