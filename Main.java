b568959c8a1424947b9c7dd4fa2bb9cf6763e5a9
Shehan Cooray <109017782+S-96-C@users.noreply.github.com>
2022-08-25 19:24:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Account acc1 = new FixedDepositAccount();//fixed deposit object
+		SavingAccount acc2 = new SavingAccount();//savingAccount object
+		
+		acc1.deposit(10000);
+		
+		acc2.deposit(5000);
+		acc2.withdraw(300.0);
+
+    acc1.display();
+		acc2.display();
+		
+  }
+
+}
---
+++ SavingAccount.java
@@ -0,0 +1,14 @@
+public class SavingAccount 	extends FixedDepositAccount{
+
+	void withdraw(double amount)
+	{
+		balance = balance - amount;
+	}
+	
+	double calculateInterest()
+	{
+		Interest = balance * interestRate/100/12;
+		return Interest;
+	}
+	
+}
